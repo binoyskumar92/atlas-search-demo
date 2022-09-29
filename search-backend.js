845c4458850c1cda5482a2dac73e57ccda24f2c6
Binoy Subhakumar <subhakum@usc.edu>
2022-09-28 23:43:56 -0500

    added posters

--- search-backend.js
+++ search-backend.js
@@ -13,40 +13,141 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
 
         app.get('/', (req, res) => {
             const collection = client.db('sample_mflix').collection('movies');
-            collection.find({}).limit(10).toArray().then(doc=>{
+            collection.find({}).limit(10).toArray().then(doc => {
                 res.send(doc);
             })
-            
-          })
+
+        })
         app.get('/search/:type/:query', async (req, res) => {
             const type = req.params['type'];
             const query = req.params['query'];
             const collection = client.db('sample_mflix').collection('movies');
-            const aggregateQuery = getSearchAggregationBasedOnType(type,query)
-            if(aggregateQuery.length>0){
-            const aggCursor = collection.aggregate(aggregateQuery);
-            
-            responseArr =[]
-            for await (const doc of aggCursor) {
-                responseArr.push(doc)
+            const aggregateQuery = getSearchAggregationBasedOnType(type, query)
+            if (aggregateQuery.length > 0) {
+                const aggCursor = collection.aggregate(aggregateQuery);
+
+                responseArr = []
+                for await (const doc of aggCursor) {
+                    responseArr.push(doc)
+                }
+                res.send(responseArr)
             }
-            res.send(responseArr)
-            }
-          })
+        })
     })
-function getSearchAggregationBasedOnType(type, query){
-const aggregation = []
-switch(type){
-    case 'Normal':
-        aggregation.push({ "$search": { "text": { "path": { "wildcard": "*" }, "query": query }}})
-        break;
-    case 'Indexed':
-        break;
-    case 'Fuzzy':
-        break;
-    case 'Boosted':
-        break;
+function getSearchAggregationBasedOnType(type, query) {
+    const aggregation = []
+    switch (type) {
+        case 'Normal':
+            aggregation.push({
+                "$search": {
+                    "text": {
+                        "path": {
+                            "wildcard": "*"
+                        },
+                        "query": query
+                    }
+                }
+            })
+            break;
+
+        case 'Indexed':
+            aggregation.push({
+                "$search": {
+                    "index": "plot",
+                    "text": {
+                        "path": ["plot"],
+                        "query": query,
+                    }
+                }
+            },
+            {
+                "$addFields":{
+                    "score":{
+                        "$meta": "searchScore"
+                    }
+                }
+            })
+            break;
+
+        case 'Fuzzy':
+            aggregation.push({
+                "$search": {
+                    "text": {
+                        "path": ["title"],
+                        "query": query,
+                        "fuzzy": {
+                            "maxEdits": 2
+                        }
+                    }
+                }
+            },{
+                "$addFields":{
+                    "score":{
+                        "$meta": "searchScore"
+                    }
+                }
+            })
+            break;
+
+        case 'Boosted':
+            aggregation.push({
+                "$search": {
+                    "compound":{
+                        "must":[{
+                            "text":{
+                                "query": query,
+                                "path": ["title"],
+                                "score": {
+                                    "boost":{
+                                        "value": 5
+                                    }
+                                }
+                            }
+                        },{
+                            "text": {
+                                "query": query,
+                                "path": ["title","plot"],
+                            }
+                        }]
+                    }
+                }
+            }, {
+                "$project": {
+                    "imdb": 1,
+                    "plot": 1,
+                    "title": 1,
+                    "genres": 1,
+                    "year": 1,
+                    "poster": 1,
+                    "score": {
+                        "$meta": "searchScore"
+                    }
+                }
+            })
+            break;
+    }
+    return aggregation
 }
-return aggregation
-}
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// const smaple = {
+//     "$search": {
+//         "compound":{
+//             "must":[{
+//                 "text":{
+//                     "query": query,
+//                     "path": ["title"],
+//                     "score": {
+//                         "boost":{
+//                             "value": 5
+//                         }
+//                     }
+//                 }
+//             },{
+//                 "text": {
+//                     "query": query,
+//                     "path": ["title","plot"],
+//                 }
+//             }]
+//         }
+//     }
+// }
--- src/html/index.js
+++ src/html/index.js
@@ -8,33 +8,43 @@ async function search() {
                 throw new Error(`HTTP error: ${response.status}`);
             }
             const data = await response.json();
-            console.log(data) 
-            let output = '';
+            console.log(data)
+            let output = ''; 
             data.forEach(e => {
                 output +=
-                    `                <div class="flex flex-col border p-2 m-1 rounded-md bg-[#ebf3d8]">
-                    <div class="flex">
-                        <label class="font-bold">Movie name:&nbsp</label>
-                        <div>${e.title}</div>
-                    </div>
-                    <div class="flex">
-                        <label class="font-bold">Plot:&nbsp</label>
-                        <div>${e.plot}</div>
-                    </div>
-                    <div class="flex">
-                        <label class="font-bold">Genre:&nbsp</label>
-                        <div>${e.genres && e.genres.join(',')}</div>
-                    </div>
-                    <div class="flex">
-                        <label class="font-bold">Year:&nbsp</label>
-                        <div>${e.year}</div>
-                    </div>
-                    <div class="flex">
-                        <label class="font-bold">IMDB rating:&nbsp</label>
-                        <div>${e.imdb.rating}</div>
-                    </div>
+`                    <div class="flex justify-between border p-3 m-1 space-x-10 rounded-md bg-[#ebf3d8]">
 
-                </div>`
+                        <div class="flex flex-col">
+                            <div class="flex">
+                                <label class="font-bold">Movie name:&nbsp</label>
+                                <div>${e.title}</div>
+                            </div>
+                            <div class="flex">
+                                <label class="font-bold">Plot:&nbsp</label>
+                                <div>${e.plot}</div>
+                            </div>
+                            <div class="flex">
+                                <label class="font-bold">Genre:&nbsp</label>
+                                <div>${e.genres && e.genres.join(',')}</div>
+                            </div>
+                            <div class="flex">
+                                <label class="font-bold">Year:&nbsp</label>
+                                <div>${e.year}</div>
+                            </div>
+                            <div class="flex">
+                                <label class="font-bold">IMDB rating:&nbsp</label>
+                                <div>${e.imdb.rating}</div>
+                            </div>
+                            <div class="flex">
+                                <label class="font-bold">Score:&nbsp</label>
+                                <div>${e.score ? e.score : 'N/A'}</div>
+                            </div>
+                        </div>
+                        <div class="flex">
+                            <img src=${e.poster?e.poster:'../static/poster-holder.jpeg'} class="w-40 h-50" />
+                        </div>
+
+                    </div>`
 
             });
             if (!output) {
@@ -58,10 +68,10 @@ function clearPage() {
     document.getElementById('data').innerHTML = ''
 }
 
-function getCheckedRadioButtonLabelValue(){
+function getCheckedRadioButtonLabelValue() {
     const radioElements = document.getElementsByName("feature-selection");
-    for(elem of radioElements){
-        if(elem.checked){
+    for (elem of radioElements) {
+        if (elem.checked) {
             return elem.labels[0].innerHTML
         }
     }
